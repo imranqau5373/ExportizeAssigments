ff7b94b5d50b034cd50143328f7adf0c59af9c37
Imran Khan <Imran.khan@synergy.it>
2018-12-21 00:23:47 +0500

    first task completed

--- Assigment 2/Program 1/script.js
+++ Assigment 2/Program 1/script.js
@@ -1,7 +1,16 @@
 
 
 window.onload = function(e){ 
-    document.getElementById("p1").innerHTML ="New text!";
+
+    let student = ['Imran','Ahmad','Hamad','Abdullah'];
+    let studentScore = [300,350,400,358];
+    let totalMarks = 500;
+    for(let i =0; i < 3; i++){
+        let studentName = student[i];
+        let obtainMarks = studentScore[i];
+        let percentage = (obtainMarks * 100)/500;
+        document.getElementById(i).innerHTML = "* Score of "+studentName+ " is "+obtainMarks+". Percentage is "+percentage+"%.";
+    }
 
 };
 
