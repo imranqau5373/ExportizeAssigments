4670935d90ebb69d8dd652ea4b350101ac293bc0
Imran Khan <Imran.khan@synergy.it>
2018-12-21 00:29:30 +0500

    2 program completed

---
+++ Assigment 2/Program 2/script.js
@@ -0,0 +1,12 @@
+
+
+window.onload = function(e){ 
+    let sum = 0;
+    for(let i =0; i <=100; i++){
+        sum = sum + i;
+      
+    }
+    document.getElementById("result").innerHTML = sum;
+
+};
+
