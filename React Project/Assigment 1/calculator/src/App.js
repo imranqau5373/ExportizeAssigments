d2e8da6a0927dc3515aa358e0bfea0755fe4fb8a
Imran Khan <Imran.khan@synergy.it>
2018-12-29 23:10:09 +0500

    Calculator Assigment

--- React Project/Assigment 1/calculator/src/App.js
+++ React Project/Assigment 1/calculator/src/App.js
@@ -1,26 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Home,Dashboard} from './home';
+import './calculator.css';
+import './calFunction.js';
+
+import {TextBox} from './textbox.js';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <p>
-            Hello to the world
           </p>
-          <Home></Home>
-          <a
+          <TextBox></TextBox>
+          {/* <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-          </a>
-          <Dashboard></Dashboard>
+            Learn React
+          </a> */}
         </header>
       </div>
     );
