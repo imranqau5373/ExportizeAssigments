dc402e47524c7a808db148af8c9b9975fd09cd00
Imran Khan <Imran.khan@synergy.it>
2019-01-21 18:41:25 +0500

    react project and its working.

--- React Project/First Project/IterateList/list/src/App.js
+++ React Project/First Project/IterateList/list/src/App.js
@@ -1,31 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Home,Dashboard} from './home';
-import AnotherHome from './AnotherHome';
-
+import NewApp from './newApp'
 const list = [ { title: 'React', url: 'https://facebook.github.io/react/', author: 'Jordan Walke', num_comments: 3, points: 4, objectID: 0, }, { title: 'Redux', url: 'https://github.com/reactjs/redux', author: 'Dan Abramov, Andrew Clark', num_comments: 2, points: 5, objectID: 1, }, ];
-
 class App extends Component {
   render() {
-    var helloWorld = 'Welcome to the Road to learn React'; 
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-          {helloWorld}
+          {list.map(function (item) 
+            { return <div>{item.title}</div>; 
+            })}
           </p>
-          <Home></Home>
           <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
+            Learn React
           </a>
-          <Dashboard></Dashboard>
-          <AnotherHome></AnotherHome>
+          <NewApp></NewApp>
         </header>
       </div>
     );
---
+++ React Project/First Project/firstproject/src/AnotherHome.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+class AnotherHome extends Component{
+    render(){
+        //some thing must be return
+        return (
+        <div>
+            <h1>this is another home.</h1>
+        </div>
+        )
+        
+    }
+}
+
+export default AnotherHome
